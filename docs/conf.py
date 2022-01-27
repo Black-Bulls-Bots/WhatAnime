5ea88a0c2a61bdb889fb3bf0e173baba02c38b87
Joker Hacker <jokerhacker.6521@protonmail.com>
2022-01-27 14:51:26 +0530

    docs: fix python version

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, Joker Hacker'
 author = 'Joker Hacker'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.1'
+release = '0.1.2b'
 
 
 # -- General configuration ---------------------------------------------------
