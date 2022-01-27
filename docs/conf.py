366bf24f1de367066ae0fcabe13835057cd91509
Joker Hacker <jokerhacker.6521@protonmail.com>
2022-01-27 14:55:54 +0530

    docs: update package version

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, Joker Hacker'
 author = 'Joker Hacker'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.2b'
+release = '0.1.2'
 
 
 # -- General configuration ---------------------------------------------------
